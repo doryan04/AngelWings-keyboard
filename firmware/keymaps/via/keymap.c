6afc59d2bc974428855a66b85bd01c131483c947
ItzAdel <gdquasar@bk.ru>
2022-09-18 19:45:05 +0400

    removed rgb customization from via in json, edited keymap via

--- firmware/keymaps/via/keymap.c
+++ firmware/keymaps/via/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_GRV,  KC_Q,    KC_W,    KC_E,          KC_R,            KC_T,               KC_Y,          KC_U,           KC_I,    KC_O,   KC_P,    KC_LBRC,         KC_RBRC,
     KC_PGUP, KC_LCTL, KC_A,    KC_S,    KC_D,          KC_F,            KC_G,               KC_H,          KC_J,           KC_K,    KC_L,   KC_SCLN, RCTL_T(KC_QUOT), KC_PGDN,
              KC_LALT, KC_Z,    KC_X,    KC_C,          KC_V,            KC_B,               KC_N,          KC_M,           KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RALT, KC_BSLS),
-                                        LT(2, KC_DEL), LSFT_T(KC_BSPC), LT(1, KC_LGUI),     LT(1, KC_ENT), RSFT_T(KC_SPC), LT(2, KC_TAB)
+                                        LT(2, KC_DEL), MT(MOD_LSFT, KC_BSPC), LT(1, KC_LGUI),     LT(1, KC_ENT), MT(MOD_RSFT, KC_SPC), LT(2, KC_TAB)
     ),
 
     [1] = LAYOUT_3x7(
